daa3477bf3c5ead2be2bce2e61888a5355de02a7
SonarJayesh <jayeshsonar2824@gmail.com>
2024-04-29 16:12:00 +0530

    [MODIFIED]: JAYESH UC-1-Completed for Create a Contact

--- src/com/jayesh/addressbook/AddressBookMain.java
+++ src/com/jayesh/addressbook/AddressBookMain.java
@@ -1,7 +1,10 @@
 package com.jayesh.addressbook;
 
 public class AddressBookMain {
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to Address Book System");
+
     }
 }
---
+++ src/com/jayesh/addressbook/Contacts.java
@@ -0,0 +1,13 @@
+package com.jayesh.addressbook;
+
+public class Contacts {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+    private String email;
+
+}
