34a322456b17459fc7cfc91511c888322681ac5a
SonarJayesh <jayeshsonar2824@gmail.com>
2024-04-29 13:28:14 +0530

    [ADDED]: JAYESH this code show Message for Welcome to Address book System

---
+++ src/com/jayesh/addressbook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.jayesh.addressbook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book System");
+    }
+}
