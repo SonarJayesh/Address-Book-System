03438eca83710e802163f466836c2696b782c3ad
SonarJayesh <jayeshsonar2824@gmail.com>
2024-04-30 12:40:15 +0530

    [MODIFIED]: JAYESH UC-3-Completed for edit Existing Contact in Address book System

--- src/com/jayesh/addressbook/AddressBookMain.java
+++ src/com/jayesh/addressbook/AddressBookMain.java
@@ -1,5 +1,6 @@
 package com.jayesh.addressbook;
 
+
 import java.util.Scanner;
 
 public class AddressBookMain {
@@ -38,11 +39,40 @@ public class AddressBookMain {
         System.out.println("Phone Number:"+contacts.getPhoneNumber());
         System.out.println("Email:"+contacts.getEmail());
     }
+
+    void editContacts(){
+        System.out.println("Enter the First Name to Edit Contact");
+        String fName = scanner.next();
+        if(fName.equals(contacts.getFirstName())) {
+            System.out.println("Contact Found.");
+
+            System.out.println("Enter the Last Name:");
+            contacts.setLastName(scanner.next());
+            System.out.println("Enter the Address:");
+            contacts.setAddress(scanner.next());
+            System.out.println("Enter the City:");
+            contacts.setCity(scanner.next());
+            System.out.println("Enter the State:");
+            contacts.setState(scanner.next());
+            System.out.println("Enter the ZipCode:");
+            contacts.setZip(scanner.next());
+            System.out.println("Enter the Phone Number:");
+            contacts.setPhoneNumber(scanner.next());
+            System.out.println("Enter the Email");
+            contacts.setEmail(scanner.next());
+
+        }else{
+            System.out.println("Given Name of Contact is Not Found");
+        }
+
+    }
     public static void main(String[] args) {
 
         System.out.println("Welcome to Address Book System");
         AddressBookMain addressBookMain = new AddressBookMain();
         addressBookMain.addContacts();
+        addressBookMain.displayContacts();
+        addressBookMain.editContacts();
         addressBookMain.displayContacts();
 
     }
